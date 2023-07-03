a1013f4d4c865686bb987a9c36e777427eebe33e
Adonai Domingez <donihernandez003@gmail.com>
2023-07-03 14:48:25 -0400

    Initial Commit

---
+++ RickAndMorty-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty-UIKit
+//
+//  Created by Doni on 7/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
